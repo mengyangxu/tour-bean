17783bb68c79be4edfa173c9033ef79639800b3b
xmy <770212572@qq.com>
2018-04-20 17:05:36 +0800

    comp

---
+++ src/main/java/com/xmy/bean/bean/Plate.java
@@ -0,0 +1,27 @@
+package com.xmy.bean.bean;
+
+/**
+ * @Description:
+ * @Author: xumengyang
+ * @Date: Created in 10:22 2018/4/20
+ */
+public class Plate {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
